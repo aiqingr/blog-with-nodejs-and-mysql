19085a1ff58141aa75860bea080b5254615f408a
Ni Tongyang <ntyaiqingr@gmail.com>
2017-04-17 18:20:44 -0400

    add the first html page

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ app.listen(4000, function() {
   console.log('listening on 4000')
 })
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
+
+app.get('/main', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.post('/quotes', (req, res) => {
+  console.log('Hellooooooooooooooooo!')
+})
+
 console.log('Start the server');
